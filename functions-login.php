00c160cf7e4e4444f989407f70e99777a852acb6
Gabriel Castillo <gabriel.castillo81@gmail.com>
2015-09-08 10:22:48 -0700

    Create custom login functions
    Load this file with your functions.php file to initialize.

---
+++ functions-login.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Add custom stylesheet to login page
+ */
+function custom_login_style() {
+	echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/css/login.css" media="all" />';
+}
+add_action( 'login_head', 'custom_login_style' );
+
+/*
+ * Change login logo's title and the URL it links to
+ */
+function custom_login_header_title( $title ) {
+	return 'Go to the TDSchoolPortraits home page';
+}
+add_filter( 'login_headertitle', 'custom_login_header_title' );
+
+function custom_login_header_url( $url ) {
+	return home_url();
+}
+add_filter( 'login_headerurl', 'custom_login_header_url' );
